9678a476329a07df53b33fa37bee1cb605d11c3a
vincent <vmcodes05@gmail.com>
2024-12-29 17:02:18 -0400

    feat: approve linea usdt

--- apps/web/src/views/Swap/V3Swap/hooks/useSwapValues.ts
+++ apps/web/src/views/Swap/V3Swap/hooks/useSwapValues.ts
@@ -14,12 +14,12 @@ const APPROVED_TOKENS: { [key: string]: { symbol: string; chainId: number } } = 
   // Mainnet
   '0xdac17f958d2ee523a2206206994597c13d831ec7': { symbol: 'USDT', chainId: 1 },
   '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': { symbol: 'USDC', chainId: 1 },
-  '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2': { symbol: 'WETH', chainId: 1 },
+  '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2': { symbol: 'WETH', chainId: 1 },
 
   // Arbitrum One
   '0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9': { symbol: 'USDT', chainId: 42161 },
   '0xaf88d065e77c8cc2239327c5edb3a432268e5831': { symbol: 'USDC', chainId: 42161 },
-  '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1': { symbol: 'WETH', chainId: 42161 },
+  '0x82af49447d8a07e3bd95bd0d56f35241523fbab1': { symbol: 'WETH', chainId: 42161 },
 
   // Base
   '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913': { symbol: 'USDC', chainId: 8453 },
@@ -27,6 +27,7 @@ const APPROVED_TOKENS: { [key: string]: { symbol: string; chainId: number } } = 
   '0x6a4e0f83d7882bcacff89aaf6f60d24e13191e9f': { symbol: 'DBRO', chainId: 8453 },
 
   // Linea
+  '0xa219439258ca9da29e9cc4ce5596924745e12b93': { symbol: 'USDT', chainId: 59144 },
   '0x176211869ca2b568f2a7d4ee941e073a821ee1ff': { symbol: 'USDC', chainId: 59144 },
   '0xe5d7c2a44ffddf6b295a15c148167daaaf5cf34f': { symbol: 'WETH', chainId: 59144 },
 }
