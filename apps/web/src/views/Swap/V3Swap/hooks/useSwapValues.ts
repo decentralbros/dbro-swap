6a72658c47a8d1b4120a22ad4afeb03b91dbc486
vincent <vmcodes05@gmail.com>
2025-01-23 11:40:40 -0400

    fix: swap fees

--- apps/web/src/views/Swap/V3Swap/hooks/useSwapValues.ts
+++ apps/web/src/views/Swap/V3Swap/hooks/useSwapValues.ts
@@ -61,7 +61,7 @@ export const useSwapValues = (): Estimate | undefined => {
       sellAmount,
       taker: account as string,
       swapFeeRecipient: FEE_ADDRESS,
-      swapFeeBps: 50, // 1%
+      swapFeeBps: 50, // 0.5%
       swapFeeToken,
       slippageBps: allowedSlippage * 100, // 100 is 1%
     }
